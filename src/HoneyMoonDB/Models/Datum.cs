a4d13f5338b85d83c6bd4011b984d73743c1c4ea
nassim <nassimancharad@gmail.com>
2017-01-08 18:40:22 +0100

    Added models, changed CSS/html

---
+++ src/HoneyMoonDB/Models/Datum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoneyMoonDB.Models
+{
+    public class Datum
+    {
+        [Key]
+        public DateTime datum { get; set; }
+        [ForeignKey("Tijden")]
+        public virtual ICollection<Tijden> AfspraakTijd { get; set; }
+
+    }
+}
