59d98fbbf7cf2227029ce325b18bc328214952f5
Nass <Nassim@DESKTOP-GSN66UU>
2017-01-19 12:57:44 +0100

    Revert "com"
    This reverts commit 6c6c0d6569093daaeb0167a51498e2d7b351380a.

---
+++ src/HoneymoonShop/Model/AppointmentModels/Appointment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoneymoonShop.Model.AppointmentModels
+{
+    public enum AppointmentType { TrouwjurkenPassen, TrouwpakkenPassen, AfspeldAfspraak}
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        [Required, Display( Name="Naam")]
+        public string Name { get; set; }
+        [Required, HiddenInput]
+        public DateTime AppointmentTime { get; set; }
+        [Required, Display(Name = "Trouwdatum")]
+        public DateTime WeddingDate { get; set; }
+        [MaxLength(15), Phone, Display( Name="Telefoonnummer")]
+        public string PhoneNumber { get; set; }
+        [Required, EmailAddress, Display(Name = "Emailadres")]
+        public string Email { get; set; }
+        [Required]
+        public AppointmentType Type { get; set; }
+
+    }
+}
